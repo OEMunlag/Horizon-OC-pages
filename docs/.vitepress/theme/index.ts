86d29106ba4b49a2b7e7a5140b01791546e8a694
OEMunlock <87287937+OEMunlag@users.noreply.github.com>
2026-02-06 23:09:04 -0300

    style.css is a much better fit

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,7 +1,7 @@
 import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
-import './custom.css'
+import './style.css'
 
 export default {
   extends: DefaultTheme,
